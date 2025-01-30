91e418448ee6615debeae9bf2efcf8100a7329b7
lorenzodelmedico <lorenzodelmedico@gmail.com>
2025-01-30 11:05:55 +0100

    v.2 added -port and -host to cli cmds

--- src/pokemon_world/app.py
+++ src/pokemon_world/app.py
@@ -1,5 +1,7 @@
+import argparse
 from flask import Flask, render_template
 import os
+
 
 def create_app():
     app = Flask(__name__,
@@ -12,9 +14,33 @@ def create_app():
     
     return app
 
-def main():
+
+def run_server(host='0.0.0.0', port=5000):
+    """Run the Flask server with specified host and port"""
     app = create_app()
-    app.run(debug=True)
+    app.run(host=host, port=port)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Pokemon World Web Application',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        default=5000,
+        help='Port to run the server on'
+    )
+    
+    parser.add_argument(
+        '-H', '--host',
+        default='0.0.0.0',
+        help='Host interface to bind the server'
+    )
+    
+    args = parser.parse_args()
+    run_server(host=args.host, port=args.port)
 
 if __name__ == '__main__':
     main()
